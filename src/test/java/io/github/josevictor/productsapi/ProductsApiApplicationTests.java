0f2a93e88bd81175babc3acb8fad764cb458b703
JoseVictorMarques <josevictor.marques1998@gmail.com>
2024-10-23 19:41:50 -0300

    first commit

---
+++ src/test/java/io/github/josevictor/productsapi/ProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.josevictor.productsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
